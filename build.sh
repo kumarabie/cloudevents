fe163a5b178aa3841405fa7107a75a8dbcf90ff1
kumarabie <kabhinav569@gmail.com>
2021-03-01 17:41:36 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -19,3 +19,12 @@ docker push kumarabby/dashdemo:0.1
 go get -u github.com/tsenart/vegeta
 docker tag local-image:tagname new-repo:tagname
 docker push new-repo:tagname
+
+
+-X POST \
+    -H "Ce-Id: 536808d3-88be-4077-9d7a-a3f162705f79" \
+    -H "Ce-Specversion: 1.0" \
+    -H "Ce-Type: dev.knative.samples.helloworld" \
+    -H "Ce-Source: dev.knative.samples/helloworldsource" \
+    -H "Content-Type: application/json" \
+    -d '{"msg":"Hello World from the curl pod."}'
